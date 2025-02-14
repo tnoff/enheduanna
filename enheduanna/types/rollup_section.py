421f06ba9159f0412578d8952c6a85829cf6b940
Tyler North <me@tyler-north.com>
2025-02-13 22:48:28 -0800

    Add rollup section assertions, add coverage build check

--- enheduanna/types/rollup_section.py
+++ enheduanna/types/rollup_section.py
@@ -1,4 +1,6 @@
-from pydantic import TypeAdapter
+from typing import Self
+
+from pydantic import TypeAdapter, model_validator
 from pydantic.dataclasses import dataclass
 
 @dataclass
@@ -9,6 +11,20 @@ class RollupSection:
     title: str
     regex: str = None
     groupBy: str = None
+
+    @model_validator(mode='after')
+    def validate_groupBy(self) -> Self:
+        '''
+        Check regex and groupBy fields are valid
+        '''
+        if self.regex and not self.groupBy:
+            raise AssertionError('Regex requires groupBy be set')
+        if self.groupBy and not self.regex:
+            raise AssertionError('GroupBy requires regex be set')
+        if self.regex and self.groupBy:
+            if f'(?P<{self.groupBy}' not in self.regex:
+                raise AssertionError('GroupBy field must be gathered in regex')
+        return self
 
     def __str__(self):
         return self.title
